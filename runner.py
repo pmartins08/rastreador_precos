66cac91477c934aa050e4cad006a1dc128fa8e9f
pmartins08 <pfonsecamartins08@gmail.com>
2026-09-08 02:31:03 +0100

    refactor: centralizar bootstrap e classificação HTTP

--- runner.py
+++ runner.py
@@ -1,8 +1,13 @@
 from brain_runtime import apply
+from response_classification import classify
 
 apply()
 
-from runner_v84 import main, score_allow_unknown
+import runner_v84
+
+runner_v84.classify = classify
+main = runner_v84.main
+score_allow_unknown = runner_v84.score_allow_unknown
 
 
 if __name__ == "__main__":
